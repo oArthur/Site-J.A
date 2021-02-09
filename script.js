eb8305c35cae8ed1b830acc5ab62b1366d0d2243
oArthur <arthursouzamaciel@gmail.com>
2021-02-09 02:03:47 -0300

    :package: Adicionado slick nos cards

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function() {
     var sidebar = document.querySelector(".container").classList.toggle("show-menu");
 });
+
+$('.projetos').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
