d6fb54ebbb02f031ab76f2aecbf8dc31308428f1
oArthur <arthursouzamaciel@gmail.com>
2021-02-09 15:52:40 -0300

    :beetle: correcao dos slide cards

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ hamburguer.addEventListener("click", function() {
 });
 
 $('.projetos').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    variableWidth: true
 });
