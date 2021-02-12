5c530e5cb81cfd46466ae117e8b4ac56d0b7f48a
oArthur <arthursouzamaciel@gmail.com>
2021-02-11 21:09:32 -0300

    :up: img contato, cards

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", function() {
-    var sidebar = document.querySelector(".container").classList.toggle("show-menu");
-});
-
-$('.projetos').slick({
+$('.ljs-parceiras').slick({
     autoplay: true,
     autoplaySpeed: 3000,
     dots: true,
-    arrows: true,
+    arrows: false,
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     adaptiveHeight: true,
     variableWidth: true
+});
+
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function() {
+    var sidebar = document.querySelector(".container").classList.toggle("show-menu");
 });
