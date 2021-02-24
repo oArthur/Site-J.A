49520ba94226a4a258090f9e10d47babb42fdd81
oArthur <arthursouzamaciel@gmail.com>
2021-02-24 18:14:41 -0300

    :up: links dos sites de clientes

--- script.js
+++ script.js
@@ -11,5 +11,23 @@ $('.ljs-parceiras').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1
+});
 
+document.getElementById('btn-card1').addEventListener('click', function() {
+    window.open('http://objetivaatacadista.com.br/', '_blank');
+});
+document.getElementById('btn-card2').addEventListener('click', function() {
+    window.open('https://campeaodaconstrucao.com.br/', '_blank');
+});
+document.getElementById('btn-card3').addEventListener('click', function() {
+    window.open('https://www.soreparos.com.br/', '_blank');
+});
+document.getElementById('btn-card4').addEventListener('click', function() {
+    window.open('http://chamaalternativa.com.br/', '_blank');
+});
+document.getElementById('btn-card5').addEventListener('click', function() {
+    window.open('http://accoelho.com.br/ac/', '_blank');
+});
+document.getElementById('btn-card6').addEventListener('click', function() {
+    window.open('https://www.condorbrasil.com.br/', '_blank');
 });
